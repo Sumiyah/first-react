5e8efa12173e043e3d925dcfd14d42852447a7e0
Sumiyah <sumiyah.fa@gmail.com>
2020-11-24 13:23:37 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 className='text-secondary'>Hello Dojo!</h1>
-      <h3 className="text-danger">Things I need to do:</h3>
-      <ul className='bg-dark text-light '>
-        <li>Get Coffee</li>
-        <li>Eat breakfast</li>
-        <li>Finish Todays assignmnets</li>
-        <li>Learn React</li>
-        <li className='text-primary'>Sleep Early!!!</li>
-      </ul>
+      <div className="card">
+        <div className="card-header">
+          <h1 className='text-secondary'>Hello Dojo!</h1>
+        </div>
+        <div className="card-body bg-dark">
+
+        <h3 className="text-danger">Things I need to do:</h3>
+        <ul className=' text-light '>
+          <li>Get Coffee</li>
+          <li>Eat breakfast</li>
+          <li>Finish Todays assignments</li>
+          <li>Learn React</li>
+          <li className='text-primary'>Sleep Early!!!</li>
+        </ul>
+        </div>
+      </div>
     </div>
   );
 }
